fedb544d08419c543bd54afaabc7da6a5983786b
saurabhpal1 <saurabhpal052002@gmail.com>
2023-02-24 23:09:13 +0530

    ECM-1 <create products>

--- database/02-create-products.sql
+++ database/02-create-products.sql
@@ -1,14 +1,10 @@
--- -----------------------------------------------------
--- Schema full-stack-ecommerce
--- -----------------------------------------------------
+
 DROP SCHEMA IF EXISTS `full-stack-ecommerce`;
 
 CREATE SCHEMA `full-stack-ecommerce`;
 USE `full-stack-ecommerce` ;
 
--- -----------------------------------------------------
--- Table `full-stack-ecommerce`.`product_category`
--- -----------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS `full-stack-ecommerce`.`product_category` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(255) NULL DEFAULT NULL,
@@ -16,9 +12,7 @@ CREATE TABLE IF NOT EXISTS `full-stack-ecommerce`.`product_category` (
 ENGINE=InnoDB
 AUTO_INCREMENT = 1;
 
--- -----------------------------------------------------
--- Table `full-stack-ecommerce`.`product`
--- -----------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS `full-stack-ecommerce`.`product` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `sku` VARCHAR(255) DEFAULT NULL,
@@ -39,9 +33,7 @@ ENGINE=InnoDB
 AUTO_INCREMENT = 1;
 
 
--- -----------------------------------------------------
--- Add sample data
--- -----------------------------------------------------
+
 
 INSERT INTO product_category(category_name) VALUES ('BOOKS');
 
