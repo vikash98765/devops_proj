c16a86caa1ea5c27212c45fc27703a3777d5bab1
vikash98765 <125661988+vikash98765@users.noreply.github.com>
2023-03-10 14:28:15 +0530

    Update ProductRepository.java

--- devopsproj/src/main/java/com/upes/devopsproj/dao/ProductRepository.java
+++ devopsproj/src/main/java/com/upes/devopsproj/dao/ProductRepository.java
@@ -1,10 +1,11 @@
 package com.upes.devopsproj.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository; 
-
 import com.upes.devopsproj.*;
 import com.upes.devopsproj.entity.Product;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository  extends JpaRepository<Product,Long> {
 	
 	
