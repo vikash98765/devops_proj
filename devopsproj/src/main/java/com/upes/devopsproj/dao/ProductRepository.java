06abb411f89777f69c9bb85ba0c0e4afd66e8b21
vikash98765 <vikashkumarvikash999@gmail.com>
2023-03-28 00:30:10 +0530

    ECM-17 <updating springboot and productservice>

--- devopsproj/src/main/java/com/upes/devopsproj/dao/ProductRepository.java
+++ devopsproj/src/main/java/com/upes/devopsproj/dao/ProductRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.upes.devopsproj.*;
 import com.upes.devopsproj.entity.Product;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository  extends JpaRepository<Product,Long> {
-	
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 	
 	
 
