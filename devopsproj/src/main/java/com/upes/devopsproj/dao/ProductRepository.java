5e37d4a3923dc3e43f513bc8090574a73fa5e3cc
vikash98765 <vikashkumarvikash999@gmail.com>
2023-04-01 08:28:30 +0530

    importing package

--- devopsproj/src/main/java/com/upes/devopsproj/dao/ProductRepository.java
+++ devopsproj/src/main/java/com/upes/devopsproj/dao/ProductRepository.java
@@ -3,6 +3,7 @@ package com.upes.devopsproj.dao;
 import org.springframework.data.jpa.repository.JpaRepository; 
 import com.upes.devopsproj.*;
 import com.upes.devopsproj.entity.Product;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
